b77164e51d4b7c04e82b72af85043360a7adcb61
jeisaRaja <jeisa.raja.jrt@gmail.com>
2024-11-29 05:29:44 +0700

    feat(card_ui): add state machine for card UI interaction
    - Implement `CardStateMachine` to manage card states (BASE, CLICKED, DRAGGING, RELEASED).
    - Create states `CardBaseState`, `CardClickState`, `CardDraggingState`, and `CardReleasedState`.
    - Add state transitions and input handling in each state.
    - Modify `CardUI` to integrate state machine logic and handle user input and transitions.
    - Update `CardUI.tscn` to include new states and the state machine.
    - Add `Hand` class to manage reparenting of cards within the UI.

--- scenes/card_ui/card_states/card_state.gd
+++ scenes/card_ui/card_states/card_state.gd
@@ -8,3 +8,24 @@ signal transition_requested(from: CardState, to: State)
 
 var card_ui: CardUI
 
+func enter() -> void:
+	pass
+
+
+func exit() -> void:
+	pass
+
+
+func on_input(_event: InputEvent) -> void:
+	pass
+
+
+func on_gui_input(_event: InputEvent) -> void:
+	pass
+
+
+func on_mouse_entered() -> void:
+	pass
+	
+func on_mouse_exited() -> void:
+	pass
